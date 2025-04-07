d73565180e053464b3d75a0056c47b1c0e3cec1f
Luis Carrion Castañeda <luiscastaneda019@gmail.com>
2025-04-07 15:33:56 -0500

    Firs commit, hello world

---
+++ src/main/java/com/prueba04/ProyectoPrueba/App.java
@@ -0,0 +1,10 @@
+package com.prueba04.ProyectoPrueba;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
